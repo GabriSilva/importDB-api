b5cd16dac209d5271c10548d7d9d981778b5e025
Neighbor Vadim <gabrielsilva200570@gmail.com>
2023-04-27 14:02:27 +0100

    commitado

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.send('Ola Amigo!');
+})
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+})
+
