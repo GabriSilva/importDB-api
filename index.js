d4ab045ac4c592e8f4c0ff858837a6b64fec4714
Neighbor Vadim <gabrielsilva200570@gmail.com>
2023-05-01 15:56:15 +0100

    version 1

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
-app.get('/api', (req, res) => {
-  res.send('Ola Amigo!');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+/* const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db',
+}); */
+
+
+app.post('/api/insert', (req, res) => {
+
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: req.body.hostname,
+    user: req.body.username,
+    password: req.body.password,
+    database: req.body.databaseName,
+  });
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Something went wrong with the database');
+      return;
+    } else {
+
+      connection.query(`SELECT TABLE_NAME FROM information_schema.tables WHERE table_type='BASE TABLE' AND table_schema='${req.body.databaseName}'`, (err, results) => {
+        if (err) {
+          res.status(500).send('Something went wrong with the getting the tables:', err);
+          return;
+        }
+
+        const promises = results.map((table) => {
+          return new Promise((resolve, reject) => {
+            connection.query(`SELECT * FROM ${table.TABLE_NAME}`, (err, results) => {
+              if (err) {
+                res.status(500).send('Something went wrong with the table:', err);
+                return;
+              }
+              resolve(results);
+            })
+          })
+        })
+
+        Promise.all(promises)
+          .then((results) => {
+            res.json(results);
+          }).catch((err) => {
+            res.status(500).send('Something went wrong with the table:', err);
+            return;
+          })
+      })
+    }
+  })
 })
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.get('/api', (req, res) => {
+  res.send({
+    message: 'Olá Amigo!',
+  });
+})
+
+const PORT = 4242;
+
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
 })
 
